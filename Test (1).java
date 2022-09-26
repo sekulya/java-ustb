2caeb8c2574ddd6946fde4274f52729a47e450e5
sekulya <52187751+sekulya@users.noreply.github.com>
2022-09-26 10:34:18 +0800

    显示桌子信息
    	显示桌子信息
    【问题描述】
    
    显示一张桌子的信息，包括桌子的形状(长方形、方形、圆形、椭圆形；使用Rect、Square、Circle、Ellipse)、腿数、高度、桌面面积。定义变量来保存桌子的信息，并显示各个信息的值。要点提示：
    
    1)  显示桌子信息：形状、腿数、高度、面积
    
    2)  注意各个变量的数据类型，桌子的形状可以用字符串String来存储
    
    【输入形式】给变量初始值：形状：方形，腿数：4，高度：80，面积：1200
    【输出形式】
    
    Shape:Rect
    
    Legs:4
    
    Hight:80
    
    Area:1200
    
    【样例输入】无
    【样例输出】无
    【样例说明】无
    【评分标准】正确10分

---
+++ Test (1).java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Shape:Rect");
+        System.out.println("Legs:4");
+        System.out.println("Hight:80");
+        System.out.println("Area:1200");
+    }
+}
+
+
+
+
+
