08e72e942000af0dce2c43e8ca99fc70555f9056
sekulya <52187751+sekulya@users.noreply.github.com>
2022-09-26 10:31:57 +0800

    显示由*号组成三角形
    	显示由*号组成三角形
    【问题描述】
    编写Java程序，显示下图所示的图形
    
    
       *
    
      ***
    
     *****
    
    *******
    
    【输入形式】无
    【输出形式】显示图形
    【输入样例】无
    【输出样例】题目图形
    【样例说明】
    “*”号前面是空格，星号后面没有字符，换到下一行
    【评分标准】
    结果完全正确得20分。

---
+++ NO5jinzita.java
@@ -0,0 +1,13 @@
+package Homework;
+
+public class NO5jinzita {
+
+	public static void main(String[] args) {
+		System.out.println("   *   ");
+		System.out.println("  ***  ");
+		System.out.println(" ***** ");
+		System.out.println("*******");
+
+	}
+}
+
