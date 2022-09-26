b2491e7294a5af6882df343142cd8bfcaa3c38bf
sekulya <52187751+sekulya@users.noreply.github.com>
2022-09-26 10:41:36 +0800

    最大公约数和最小公倍数
    最大公约数和最小公倍数
    【问题描述】输入两个正整数a和b（0<a，b<1000000），求出其最大公约数和最小公倍数并输出。
    【输入文件】从标准输入读取一行，是两个整数a和b，以空格分隔。
    【输出文件】向标准输出打印以空格分隔的两个整数，分别是a、b的最大公约数和最小公倍数。在输出末尾要有一个回车符。
    【输入样例】12 18
    【输出样例】6 36
    【样例说明】12和18的最大公约数是6，最小公倍数是36.
    【评分标准】结果正确则该测试点得满分，否则该测试点得0分。

---
+++ work.java
@@ -0,0 +1,29 @@
+package homework;
+
+import java.util.Scanner;
+
+public class work {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		int min=Math.min(a, b) ;         
+		for(int i=min;i>0;i--){     
+			if(a%i==0&&b%i==0){   
+				min=i;
+				break;         
+				}	
+		}
+		int max=a*b/min;       
+		System.out.println(min+" "+max);
+		
+
+
+	}
+
+}
+
+
