8f936bd9672a64f0a5f949cb6d08d0a2a63344c1
sekulya <52187751+sekulya@users.noreply.github.com>
2022-09-26 10:55:07 +0800

    求水仙花数
    	求水仙花数
    【问题描述】编写一个程序，输入一个正整数N(N大于等于100小于等于999)，求出100~N之间的所有水仙花数。所谓  的水仙花数是：如果一个三位数的个位数、十位数、百位数的立方和等于该数自身，就称这个数为水仙花数。
    【输入形式】输入一个正整数N。
    【输出形式】输出从100到N之间的所有水仙花数，每个数以回车结束。
    【样例输入】
     400
    【样例输出】
     153
     370
     371
    【样例说明】输入一个的正整数400，输出为100~400之间的水仙花数153，370，371
    【评分标准】结果完全正确得20分，每个测试点4分。

---
+++ work2.java
@@ -0,0 +1,22 @@
+package homework;
+
+import java.util.Scanner;
+
+public class work2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner N=  new Scanner(System.in);
+		int n = N.nextInt();
+		for( int i=100;i<=n;i++){
+			 int a =i/100;
+	         int b =i/10%10;
+	         int c =i%10;
+	            if(i==a*a*a+b*b*b+c*c*c)
+	                System.out.println(i);
+		}
+		
+	}
+}
+
+
